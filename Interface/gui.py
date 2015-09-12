850bfa592da52bde2627e409784e7ef28c13dc30
Pieter <PNdeRooij@gmail.com>
2015-09-12 17:07:11 +0200

    First (true) commit! Initial segregation of program components and some files to start off with.

---
+++ Interface/gui.py
@@ -0,0 +1,19 @@
+__author__ = 'tom, stef, pieter'
+
+"""
+The super file which is above and beyond everything happening visually.
+Of course a multi-line comment is only justified if it contains multiple lines.
+"""
+
+class GUI:
+
+    # Class for graphical shizzle.
+    # It is initialized by a master call and updated every cycle.
+
+    # Initialize grid and such
+    def __init__(self):
+        pass
+
+    # An update for every cycle?
+    def update(self):
+        pass
---
+++ Simulation/master.py
@@ -0,0 +1,34 @@
+__author__ = 'tom, stef, pieter'
+
+"""
+This class contains the main loop of the simulation.
+It ensures everything is set-up properly, then it starts the simulation
+and runs it in cycles while handling the transitions between those.
+"""
+
+############################
+"These are the functions to rule them all."
+############################
+
+class Simulation:
+
+    # Class containing the entire simulation.
+    # Everything is run by a subsequent call by this class.
+
+    # Initialize the simulation
+    def __init__(self):
+        pass
+
+    # Run the simulation
+    def run(self):
+        pass
+
+    # Perhaps something to leave the user a neat and tidy system
+    def quit(self):
+        pass
+
+# Initialize a simulation instance and call its run function
+if __name__ == '__main__':
+    sim = Simulation()
+    sim.run()
+    sim.quit()
