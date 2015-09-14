38e27f0fa53e4ea4d79721717e7ddda6bc86601a
Kanji Origin <kanjiorigin@outlook.com>
2015-09-14 22:59:23 +0200

    renamed to ost-test to prevent git to see it as subproject

---
+++ osc-test/main.py
@@ -0,0 +1,89 @@
+__version__ = '0.1'
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.lib import osc
+from kivy.clock import Clock
+from kivy.utils import platform
+platform = platform()
+
+kv = '''
+BoxLayout:
+    orientation: 'vertical'
+    BoxLayout:
+        size_hint_y: None
+        height: '30sp'
+        Button:
+            text: 'start service'
+            on_press: app.start_service()
+        Button:
+            text: 'stop service'
+            on_press: app.stop_service()
+
+    ScrollView:
+        Label:
+            id: label
+            size_hint_y: None
+            height: self.texture_size[1]
+            text_size: self.size[0], None
+
+    BoxLayout:
+        size_hint_y: None
+        height: '30sp'
+        Button:
+            text: 'ping'
+            on_press: app.send()
+        Button:
+            text: 'message'
+            on_press: app.mssend()
+        Button:
+            text: 'clear'
+            on_press: label.text = ''
+        Label:
+            id: date
+
+'''
+
+
+class ClientServerApp(App):
+    def build(self):
+        self.service = None
+        self.start_service()
+        osc.init()
+        oscid = osc.listen(port=3002)
+        osc.bind(oscid, self.display_message, '/message')
+        osc.bind(oscid, self.date, '/date')
+        Clock.schedule_interval(lambda *x: osc.readQueue(oscid), 0)
+        self.root = Builder.load_string(kv)
+        return self.root
+
+    def start_service(self):
+        if platform == 'android':
+            from android import AndroidService
+            service = AndroidService('my pong service', 'running')
+            service.start('service started')
+            self.service = service
+
+    def stop_service(self):
+        if self.service:
+            self.service.stop()
+            self.service = None
+
+    def send(self, *args):
+        osc.sendMsg('/ping', [], port=3000)
+
+    def mssend(self, *args):
+        osc.sendMsg('/printy', ['hello world', ], port=3000)
+
+    def display_message(self, message, *args):
+        if self.root:
+            print(message)
+            self.root.ids.label.text += '%s\n' % message[2]
+
+    def date(self, message, *args):
+        if self.root:
+            self.root.ids.date.text = message[2]
+
+
+if __name__ == '__main__':
+    ClientServerApp().run()
---
+++ osc-test/service/responder.py
@@ -0,0 +1,34 @@
+from kivy.lib import osc
+from random import sample, randint
+from string import ascii_letters
+from time import localtime, asctime, sleep
+
+
+def ping(*args):
+    osc.sendMsg(
+        '/message',
+        [''.join(sample(ascii_letters, randint(10, 20))), ],
+        port=3002)
+
+
+def printy(mesg, *args):
+    print(mesg)
+    osc.sendMsg(
+        '/message',
+        [mesg[2], ],
+        port=3002)
+
+
+def send_date():
+    osc.sendMsg('/date', [asctime(localtime()), ], port=3002)
+
+
+if __name__ == '__main__':
+    osc.init()
+    oscid = osc.listen(ipAddr='0.0.0.0', port=3000)
+    osc.bind(oscid, ping, '/ping')
+    osc.bind(oscid, printy, '/printy')
+    while True:
+        osc.readQueue(oscid)
+        send_date()
+        sleep(.1)
