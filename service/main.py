822c965a12c5217f5d5078b4b064fe8bebd8c143
Kanji Origin <kanjiorigin@outlook.com>
2015-09-24 14:37:02 +0200

    GUI parameter settings are finished!

--- service/main.py
+++ service/main.py
@@ -2,6 +2,7 @@
 
 __author__ = 'tom, stef, pieter'
 
+from sys import exit
 import osc  # from kivy.lib import osc
 from time import sleep
 from model import Model
@@ -24,6 +25,7 @@ class Simulation():
         print("\nInit master...")
         # Build model
         self.model = Model()
+        self.agent_action = []
 
         # Initiate zeroth cycle
         self.cycle = 0
@@ -76,37 +78,59 @@ class osc_message():
 
         # Code always running / waiting for instructions
         while True:
-            print("Current cycle: {}".format(self.cycle))
-            osc.readQueue(oscid)
-            # Only run simulation when started and max cycles not reached
-            #print("Start: {}, Max Cycles: {}".format(self.can_start, self.max_cycles))
-            if self.can_start == True and self.cycle < self.max_cycles:
-                #self.sim.run()
+            # Try so that a server stopped message can be send
+            try:
+                print("Current cycle: {}".format(self.cycle))
+                osc.readQueue(oscid)
+                # Only run simulation when started and max cycles not reached
+                #print("Start: {}, Max Cycles: {}".format(self.can_start, self.max_cycles))
+                if self.can_start == True and self.cycle < self.max_cycles:
+                    #self.sim.run()
 
-                # Hello world test
-                self.send_hello()
-                self.cycle += 1
+                    # Hello world test
+                    self.cycle += 1
+                    self.send_hello()
 
-            sleep(0.5)
+                sleep(0.5)
+
+            # Send message server stopped
+            except (KeyboardInterrupt, SystemExit):
+                self.send_server_stop()
+                exit()
 
     # Change simulation status based on message from listener
     def startquit(self, can_start, *args):
         if can_start[2] == True:
             print("\n    Simulation started    ")
-        else:
+            print(can_start)
+            print("params: can_start: {}, max_cycle: {}, agent_num: {}, grid: {}x{}"
+                  .format(can_start[2], can_start[3], can_start[4], can_start[5], can_start[6]))
+            self.can_start = True
+            self.max_cycles = can_start[3]
+            self.agent_num = can_start[4]
+            self.grid = [can_start[5], can_start[6]]
+        elif can_start[2] == False:
             print("\n    Simulation ended    ")
-        self.can_start = can_start[2]
-        self.cycle = 0
+            self.cycle = 0
+            self.can_start = False
+        else:
+            print("\n    Simulation paused    ")
+            self.can_start = False
 
     # Informs the listener whether the simulation is running or not
     def simulation_status_send(self, *args):
         print("Send simulation status")
-        osc.sendMsg('/simu-status', [self.can_start, ], port=3002)
+        msg = []
+        osc.sendMsg('/simu-status', [self.can_start, self.cycle, ], port=3002)
 
     # Send text message "Hello world" to listener
     def send_hello(self, *args):
-        print("sending hello...")
-        osc.sendMsg('/receive_hello', ["Hello world", ], port=3002)
+        #print("sending cycle")
+        osc.sendMsg('/receive_hello', [self.cycle, ], port=3002)
+
+    # Send message server stopped
+    def send_server_stop(self, *args):
+        osc.sendMsg('/server-stop', port=3002)
 
 # Initialize a server instance for agent simulation
 if __name__ == '__main__':
