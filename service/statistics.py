ac69b3e3ed99ec33bde643c969853fb4ec85f71f
Tom-AI <trsrenkema@gmail.com>
2015-10-06 20:10:38 +0200

    Minor changes for transfer to other pc

---
+++ service/statistics.py
@@ -0,0 +1,32 @@
+__author__ = 'tom, stef, pieter'
+
+class Statistics:
+
+    def __init__(self):
+
+
+
+
+
+""" to log:
+
+number of agents in the grid
+
+For each crossing:
+current movement of agent
+total number of agents present
+
+
+For each grid point:
+number of agents in queue
+number of agents crossed
+number of agents waited
+
+
+"""
+
+
+
+
+
+
