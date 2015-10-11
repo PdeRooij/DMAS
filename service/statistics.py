10783f500ec3bb6989025ad96569aaa38e18a588
Tom-AI <trsrenkema@gmail.com>
2015-10-11 15:23:36 +0200

    Some temporary debugging changes concerning to test the driver list of Driver() classes.

--- service/statistics.py
+++ service/statistics.py
@@ -12,24 +12,26 @@ class Statistics:
         # Copy list of drivers
         self.drivers = driver_list
         # Reset values
-        self.stats = [0, 0, 0 ,0, 0, 0]
+        self.stats = [1, 1, 1 ,1, 1, 1]
         # Iterate over list of drivers and do statistics
         for driver in self.drivers:
             # Check if driver gave way to the left or did not
-            if driver.view == [1, 0, 0] and driver.act == 'go':
+            # Check if view == [1, 0, 0]
+            if driver.view[0] == 1 and driver.view[2] == 0 and driver.act == 'go':
                 self.stats[0] += 1
-            elif driver.view == [1, 0, 0] and driver.act == 'wait':
+            elif driver.view[0] == 1 and driver.view[2] == 0 and driver.act == 'wait':
                 self.stats[2] += 1
             # Check if driver gave way to the right or did not
-            elif driver.view == [0, 0, 1] and driver.act == 'go':
+            # Check if view == [0, 0, 1]
+            elif driver.view[0] == 0 and driver.view[2] == 1 and driver.act == 'go':
                 self.stats[1] += 1
-            elif driver.view == [0, 0, 1] and driver.act == 'wait':
+            elif driver.view[0] == 1 and driver.view[2] == 0 and driver.act == 'wait':
                 self.stats[3] += 1
 
         # Calculate ratio of agent going
-        self.stats[4] = float(self.stats[0] / (self.stats[0] + self.stats[1]))
+        self.stats[4] = self.stats[0] / float(self.stats[0] + self.stats[1])
         # Calculate ratio of agent waiting
-        self.stats[5] = float(self.stats[2] / (self.stats[2] + self.stats[3]))
+        self.stats[5] = self.stats[2] / float(self.stats[2] + self.stats[3])
 
         # Return list of statistics
         return self.stats
